84f1ce94378f11225f908e67661b132bc0fde167
Anish P Krishnan <86202381+Anish5678@users.noreply.github.com>
2024-06-12 16:04:11 +0530

    Lab-03 File - getAllExpenseItems added

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,21 +1,26 @@
 
+
 import { useEffect } from "react";
 import { getAllExpenseItems } from "../services/expense_utils";
+
+
 
 
 const Home = () => {
 
     useEffect( () => {
  
-      const getAllItemsInvoker = async () => {
- 
-        const response = await getAllExpenseItems();
-        console.log("Response is " + JSON.stringify(response));
-      }
- 
-      getAllItemsInvoker();
- 
-    }, [])
+        const getAllItemsInvoker = async () => {
+   
+          const response = await getAllExpenseItems();
+          console.log("Response is " + JSON.stringify(response));
+        }
+   
+        getAllItemsInvoker();
+   
+      }, [])
+  
+    
 
     return (
 
--- client/src/services/expense_utils.js
+++ client/src/services/expense_utils.js
@@ -1,16 +1,17 @@
-import axios from "axios";
+import React from 'react';
+import axios from "axios"
 
-// usage of axios -  a library to interact with server
+
 
 const getAllExpenseItems = async () => {
  
-    // server url
+  
   const getItemsUrl = "http://localhost:4000/expenses";
  
-  // perform Invocation
+ 
   console.log("Final Url is " + getItemsUrl);
  
-  // response
+ 
   const response = await axios.get(getItemsUrl);
   return response.data;
 
