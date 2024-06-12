05b55cab7bb2ae3feef0d9793478b2a226a771be
Anish P Krishnan <86202381+Anish5678@users.noreply.github.com>
2024-06-12 14:40:14 +0530

    Lab-04 Files - getAllExpenseItems

---
+++ client/src/services/expense_utils.js
@@ -0,0 +1,19 @@
+
+
+
+const getAllExpenseItems = () => {
+
+// install axios
+
+// usage of axios -  a library to interact with server
+
+
+// serer url
+
+// perform Invocation
+
+// response
+
+}
+
+export {getAllExpenseItems}
